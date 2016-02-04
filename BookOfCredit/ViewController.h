98bb3a53743d68a50ee1a33b9f566d8fca592043
Alexandr <Alexandr@macbook-pro-alexandr.local>
2016-02-04 16:59:52 +0300

    Initial Commit

---
+++ BookOfCredit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BookOfCredit
+//
+//  Created by Alexandr on 04.02.16.
+//  Copyright © 2016 Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
